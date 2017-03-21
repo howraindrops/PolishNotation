11d307c424a21bce4f89e1967aa3aa1127f95111
YufenMacbook <wyufen@outlook.com>
2017-03-21 20:07:49 +0800

    first commit

---
+++ src/huawei/JavaTest.java
@@ -0,0 +1,62 @@
+package huawei;
+
+import java.util.Stack;
+
+/**
+ * 请注意不要修改包名、类名，否则将导致考试成绩失效
+ */
+public class JavaTest {
+	public static class JavaTestException extends Exception {
+		private static final long serialVersionUID = 1584895723070259373L;
+	}
+
+	/**
+	 * 逆波兰表达式中存在语法错误
+	 */
+	public static class RPNSyntaxErrorException extends JavaTestException {
+		private static final long serialVersionUID = 9109750478481891787L;
+
+		public RPNSyntaxErrorException() {
+		}
+	}
+
+	/**
+	 * 逆波兰表达式计算中出现除零错误
+	 */
+	public static class DevideByZeroException extends JavaTestException {
+		private static final long serialVersionUID = 2599554592065887836L;
+
+		public DevideByZeroException() {
+		}
+	}
+
+	/**
+	 * 请实现calculate函数，来计算逆波兰表达式，
+	 * 可以根据需要增加变量和函数， 但是注意不要修改calculate函数的原型
+	 * 
+	 * @param rpn
+	 *            逆波兰表达式字符串
+	 * @return 结果字符串
+	 */
+	public double calculate(String rpn) throws JavaTestException {
+		Stack stack = new Stack();
+		String[] items = rpn.split(" ");
+		for(String item : items)
+		{
+			
+		}
+		
+		return 3.0;
+	}
+
+	public static void main(String[] args) {
+		try {
+			JavaTest c = new JavaTest();
+
+			if (args.length == 1) {
+				System.out.print(c.calculate(args[0]));
+			}
+		} catch (JavaTestException e) {
+		}
+	}
+}
---
+++ src/testcase/JavaTestTest.java
@@ -0,0 +1,24 @@
+package testcase;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import huawei.JavaTest;
+
+public class JavaTestTest extends JavaTest {
+	final static double Delta = 1e-6;  
+	
+	@Test
+	public void test_Case1() throws JavaTestException {
+		assertEquals(calculate("1.0 2.0 +"), 3.0, Delta);
+	}
+
+	@Test
+	public void test_Case2() throws JavaTestException {
+		assertEquals(calculate("1 2 + 3 *"), 9.0, Delta);
+	}
+
+	@Test(expected=RPNSyntaxErrorException.class)
+	public void test_Case3() throws JavaTestException {
+		calculate("1.0 +");
+	}
+}
